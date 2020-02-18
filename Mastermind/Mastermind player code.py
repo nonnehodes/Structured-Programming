966f0e4f4aff30bb2a22702fd41683caac74044b
nonnehodes <nonne.hodes@student.hu.nl>
2020-02-18 23:17:16 +0100

    verwijderen niet gebruikte bestanden

--- Mastermind/Mastermind player code.py
+++
@@ -1,5 +0,0 @@
-def mastermind_player_code(): #Hier moet de computer de code van de speler raden
-    Kleur1 = input('Kies je 1e kleur: ')
-    Kleur2 = input('Kies je 2e kleur: ')
-    Kleur3 = input('Kies je 3e kleur: ')
-    Kleur4 = input('Kies je 4e kleur: ')
