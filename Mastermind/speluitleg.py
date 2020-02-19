43f5f59277dbdc83d10168f317db259fd4aaa132
nonnehodes <nonne.hodes@student.hu.nl>
2020-02-19 21:23:05 +0100

    BUG FREE

--- Mastermind/speluitleg.py
+++ Mastermind/speluitleg.py
@@ -7,9 +7,10 @@ speluitleg = 'Spelregels als gamemaker:\n ' \
              'Je hebt de keuze uit de kleuren: rood, oranje, geel, groen, blauw en paars\n' \
              'De computer maakt een code van 4 kleuren, met de bovenstaande kleuren \n' \
              '(let op, kleuren kunnen meerdere keren voorkomen!)\n' \
-             'Nadat de code is bedacht kan je zelf gaan gokken, bij elke gok krijg je een hint, die bestaan uit witte en zwarte pinnen \n' \
-             'Witte pin: Er ligt een juiste kleur op de juiste plek\n' \
-             'Zwarte pin: Er ligt een juiste kleur op de verkeerde plek\n' \
+             'Nadat de code is bedacht kan je zelf gaan gokken, bij elke gok krijg je een hint, deze wod als volgt geprint; \n' \
+             '(x1,x2)\n' \
+             'x1: is de Witte pin: Er ligt een juiste kleur op de juiste plek\n' \
+             'x2 is de Zwarte pin: Er ligt een juiste kleur op de verkeerde plek\n' \
              'Als de code niet gegokt is na 10 gokken is het spel voorbij en heeft de gokker verloren. ' \
              '\n'
 
